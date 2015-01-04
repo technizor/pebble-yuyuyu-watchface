ee4421f1f449b0bd0a3715bf6ec3ecf13f705181
Sherman Ying <idiotioc@gmail.com>
2015-01-04 16:40:33 -0500

    Added functional time and date
    New font: Terminus, used for Date/Time display

--- src/main.c
+++ src/main.c
@@ -1,37 +1,104 @@
 #include <pebble.h>
-
-Window *window;
-TextLayer *text_layer;
+  
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
-void handle_init(void) {
-  window = window_create();
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
 
+  // Create a long-lived buffer
+  static char buffer[] = "00:00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    //Use 2h hour format
+    strftime(buffer, sizeof("00:00:00"), "%H:%M:%S", tick_time);
+  } else {
+    //Use 12 hour format
+    strftime(buffer, sizeof("00:00:00"), "%I:%M:%S", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+  
+  static char buffer2[] = "Sun Jan 01";
+  strftime(buffer2, sizeof("Sun Jan 01"), "%a %b %d", tick_time);
+  text_layer_set_text(s_date_layer, buffer2);
+}
+
+static void main_window_load(Window *window) {
+  
   // Background Image
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_i_yuyuyu_back);
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 125, 144, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00:00");
+  text_layer_set_font(s_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TERMINUS_NUMBERS_32)));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  // Create date TextLayer
+  s_date_layer = text_layer_create(GRect(0, 5, 144, 50));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text(s_date_layer, "Sun Jan 01");
+  text_layer_set_font(s_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TERMINUS_24)));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
   
-  // Time
-  text_layer = text_layer_create(GRect(0, 0, 144, 20));
-  
-  // Date
-  
-  window_stack_push(window, true);
+  // Make sure the time is displayed from the start
+  update_time();
 }
 
-void handle_deinit(void) {
-  
-  text_layer_destroy(text_layer);
-  gbitmap_destroy(s_background_bitmap);
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
   bitmap_layer_destroy(s_background_layer);
-  window_destroy(window);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+  
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+  
+  // Register with TickTimerService
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
 }
 
 int main(void) {
-  handle_init();
+  init();
   app_event_loop();
-  handle_deinit();
+  deinit();
 }
+
