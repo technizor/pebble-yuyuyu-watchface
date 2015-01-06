f81671c17bbd8cd224935f3ceb69ab659c0622f4
Sherman Ying <idiotioc@gmail.com>
2015-01-06 04:12:49 -0500

    GUI Overhauled
    Fixed the problem with image loading by using dynamic loading when necessary.

--- src/main.c
+++ src/main.c
@@ -1,30 +1,81 @@
 #include <pebble.h>
+
+static uint8_t m01[10]={
+RESOURCE_ID_i_karin_m01a,
+RESOURCE_ID_i_karin_m01b,
+RESOURCE_ID_i_karin_m01c,
+RESOURCE_ID_i_karin_m01d,
+RESOURCE_ID_i_karin_m01e,
+RESOURCE_ID_i_karin_m01f,
+RESOURCE_ID_i_karin_m01g,
+RESOURCE_ID_i_karin_m01h,
+RESOURCE_ID_i_karin_m01i,
+RESOURCE_ID_i_karin_m01j
+};
+static uint8_t m10[6]={
+RESOURCE_ID_i_karin_m10a,
+RESOURCE_ID_i_karin_m10b,
+RESOURCE_ID_i_karin_m10c,
+RESOURCE_ID_i_karin_m10d,
+RESOURCE_ID_i_karin_m10e,
+RESOURCE_ID_i_karin_m10f
+};
+static uint8_t s01[10]={
+RESOURCE_ID_i_karin_s01a,
+RESOURCE_ID_i_karin_s01b,
+RESOURCE_ID_i_karin_s01c,
+RESOURCE_ID_i_karin_s01d,
+RESOURCE_ID_i_karin_s01e,
+RESOURCE_ID_i_karin_s01f,
+RESOURCE_ID_i_karin_s01g,
+RESOURCE_ID_i_karin_s01h,
+RESOURCE_ID_i_karin_s01i,
+RESOURCE_ID_i_karin_s01j
+};
+static uint8_t s10[6]={
+RESOURCE_ID_i_karin_s10a,
+RESOURCE_ID_i_karin_s10b,
+RESOURCE_ID_i_karin_s10c,
+RESOURCE_ID_i_karin_s10d,
+RESOURCE_ID_i_karin_s10e,
+RESOURCE_ID_i_karin_s10f
+};
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
-//*
+
 static BitmapLayer *s_character_layer;
 static BitmapLayer *s_flower_base_layer;
 static GBitmap *s_character_bitmap;
 static GBitmap *s_flower_base_bitmap;
-/*
+
 static BitmapLayer *s_flower_anim_layer_s01;
 static BitmapLayer *s_flower_anim_layer_s10;
 static BitmapLayer *s_flower_anim_layer_m01;
 static BitmapLayer *s_flower_anim_layer_m10;
+/*
+static GBitmap *s_flower_anim_bitmap_s01[10];
+static GBitmap *s_flower_anim_bitmap_s10[6];
+static GBitmap *s_flower_anim_bitmap_m01[10];
+static GBitmap *s_flower_anim_bitmap_m10[6];
+//*/
+
 static GBitmap *s_flower_anim_bitmap_s01;
 static GBitmap *s_flower_anim_bitmap_s10;
 static GBitmap *s_flower_anim_bitmap_m01;
 static GBitmap *s_flower_anim_bitmap_m10;
-//*/
-/*
+
 static uint lastSec;
 static uint lastMin;
+/*
+void  loadS01();
+void  loadS10();
+ void loadM01();
+ void loadM10();
 //*/
-
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
@@ -41,7 +92,7 @@ static void update_time() {
     //Use 12 hour format
     strftime(buffer, sizeof("00:00:00"), "%I:%M:%S", tick_time);
   }
-
+  
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
   
@@ -50,18 +101,42 @@ static void update_time() {
   text_layer_set_text(s_date_layer, buffer2);
   
   // Update flower animations
- /* uint thisSec = tick_time->tm_sec;
+  uint thisSec = tick_time->tm_sec;
   uint thisMin = tick_time->tm_min;
-  if(lastSec%10 != thisSec%10)
-  bitmap_layer_set_bitmap(s_flower_anim_layer_s01,gbitmap_create_as_sub_bitmap(s_flower_anim_bitmap_s01, GRect(0,(thisSec%10)*84,84,84)));
-  if(lastSec/10 != thisSec/10)
-       bitmap_layer_set_bitmap(s_flower_anim_layer_s10,gbitmap_create_as_sub_bitmap(s_flower_anim_bitmap_s10, GRect(0,(thisSec/10)*84,84,84))); 
-  if(lastMin%10!=thisMin%10)
-     bitmap_layer_set_bitmap(s_flower_anim_layer_m01,gbitmap_create_as_sub_bitmap(s_flower_anim_bitmap_m01, GRect(0,(thisMin%10)*84,84,84))); 
-  if(lastMin/10!=thisMin/10)
-    bitmap_layer_set_bitmap(s_flower_anim_layer_m10,gbitmap_create_as_sub_bitmap(s_flower_anim_bitmap_m10, GRect(0,(thisMin/10)*84,84,84)));
+  /*
+  bitmap_layer_set_bitmap(s_flower_anim_layer_s01,s_flower_anim_bitmap_s01[thisSec%10]);
+  bitmap_layer_set_bitmap(s_flower_anim_layer_s10,s_flower_anim_bitmap_s10[thisSec/10]);
+  bitmap_layer_set_bitmap(s_flower_anim_layer_m01,s_flower_anim_bitmap_m01[thisMin%10]); 
+  bitmap_layer_set_bitmap(s_flower_anim_layer_m10,s_flower_anim_bitmap_m10[thisMin/10]);
+  //*/
+  //*
+  if(thisSec%10 != lastSec%10)
+    {
+  gbitmap_destroy(s_flower_anim_bitmap_s01);
+  s_flower_anim_bitmap_s01 = gbitmap_create_with_resource(s01[thisSec%10]);
+  bitmap_layer_set_bitmap(s_flower_anim_layer_s01, s_flower_anim_bitmap_s01);
+  }
+  if(thisSec/10 != lastSec/10)
+    {
+  gbitmap_destroy(s_flower_anim_bitmap_s10);
+  s_flower_anim_bitmap_s10 = gbitmap_create_with_resource(s10[thisSec/10]);
+  bitmap_layer_set_bitmap(s_flower_anim_layer_s10, s_flower_anim_bitmap_s10);
+  }
+  if(thisMin%10 != lastMin%10)
+    {
+  gbitmap_destroy(s_flower_anim_bitmap_m01);
+  s_flower_anim_bitmap_m01 = gbitmap_create_with_resource(m01[thisMin%10]);
+  bitmap_layer_set_bitmap(s_flower_anim_layer_m01, s_flower_anim_bitmap_m01);
+  }
+  if(thisMin/10 != lastMin/10)
+    {
+  gbitmap_destroy(s_flower_anim_bitmap_m10);
+  s_flower_anim_bitmap_m10 = gbitmap_create_with_resource(m10[thisMin/10]);
+  bitmap_layer_set_bitmap(s_flower_anim_layer_m10, s_flower_anim_bitmap_m10);
+  }
   lastSec = thisSec;
-  lastMin = thisMin;*/
+  lastMin = thisMin;
+  //*/
 }
 
 static void main_window_load(Window *window) {
@@ -75,48 +150,28 @@ static void main_window_load(Window *window) {
   s_character_layer = bitmap_layer_create(GRect(0,42,60,84));
   bitmap_layer_set_compositing_mode(s_character_layer,GCompOpAnd);
   bitmap_layer_set_bitmap(s_character_layer,s_character_bitmap);
-  //* 
   // Flower Base Image
   s_flower_base_bitmap = gbitmap_create_with_resource(RESOURCE_ID_i_flower_base_karin);
   s_flower_base_layer = bitmap_layer_create(GRect(60,42,84,84));
-  bitmap_layer_set_compositing_mode(s_flower_base_layer,GCompOpAnd);
   bitmap_layer_set_bitmap(s_flower_base_layer,s_flower_base_bitmap);
-  
-  // Load Flower Animations
-  /*
-  s_flower_anim_bitmap_s01 = gbitmap_create_with_resource(RESOURCE_ID_i_flower_anim_karin_s01);
-  s_flower_anim_bitmap_s10 = gbitmap_create_with_resource(RESOURCE_ID_i_flower_anim_karin_s10);
-  s_flower_anim_bitmap_m01 = gbitmap_create_with_resource(RESOURCE_ID_i_flower_anim_karin_m01);
-  s_flower_anim_bitmap_m10 = gbitmap_create_with_resource(RESOURCE_ID_i_flower_anim_karin_m10);
-  //*/
+ 
   // Create Animation Layers
-  /*
+  //*
   s_flower_anim_layer_s01 = bitmap_layer_create(GRect(60,42,84,84));
   s_flower_anim_layer_s10 = bitmap_layer_create(GRect(60,42,84,84));
   s_flower_anim_layer_m01 = bitmap_layer_create(GRect(60,42,84,84));
   s_flower_anim_layer_m10 = bitmap_layer_create(GRect(60,42,84,84));
+  //*/
+  /*
+  s_flower_anim_layer_s01 = bitmap_layer_create(GRect(0,-6,84,84));
+  s_flower_anim_layer_s10 = bitmap_layer_create(GRect(84,-6,84,84));
+  s_flower_anim_layer_m01 = bitmap_layer_create(GRect(0,66,84,84));
+  s_flower_anim_layer_m10 = bitmap_layer_create(GRect(84,66,84,84));
+  //*/
   bitmap_layer_set_compositing_mode(s_flower_anim_layer_s01,GCompOpAnd);
   bitmap_layer_set_compositing_mode(s_flower_anim_layer_s10,GCompOpAnd);
   bitmap_layer_set_compositing_mode(s_flower_anim_layer_m01,GCompOpAnd);
   bitmap_layer_set_compositing_mode(s_flower_anim_layer_m10,GCompOpAnd);
-  //*/
-  /*
-  bitmap_layer_set_bitmap(s_flower_anim_layer_s01,s_flower_anim_bitmap_s01); 
-  bitmap_layer_set_bitmap(s_flower_anim_layer_s10,s_flower_anim_bitmap_s10); 
-  bitmap_layer_set_bitmap(s_flower_anim_layer_m01,s_flower_anim_bitmap_m01); 
-  bitmap_layer_set_bitmap(s_flower_anim_layer_m10,s_flower_anim_bitmap_m10);
-  //*/
-  /*
-  GBitmap *a = gbitmap_create_as_sub_bitmap(gbitmap_create_with_resource(RESOURCE_ID_i_flower_anim_karin_s01), GRect(0,0,84,84));
-  GBitmap *b = gbitmap_create_as_sub_bitmap(gbitmap_create_with_resource(RESOURCE_ID_i_flower_anim_karin_s10), GRect(0,0,84,84));
-  GBitmap *c = gbitmap_create_as_sub_bitmap(gbitmap_create_with_resource(RESOURCE_ID_i_flower_anim_karin_m01), GRect(0,0,84,84));
-  GBitmap *d = gbitmap_create_as_sub_bitmap(gbitmap_create_with_resource(RESOURCE_ID_i_flower_anim_karin_m10), GRect(0,0,84,84));
-  
-  bitmap_layer_set_bitmap(s_flower_anim_layer_s01,a); 
-  bitmap_layer_set_bitmap(s_flower_anim_layer_s10,b); 
-  bitmap_layer_set_bitmap(s_flower_anim_layer_m01,c); 
-  bitmap_layer_set_bitmap(s_flower_anim_layer_m10,d); 
-  //*/
   
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 125, 144, 50));
@@ -135,19 +190,23 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
+  //*
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_character_layer));
+  
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_flower_base_layer));
-  /*
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_flower_anim_layer_s01));
-  layer_add_child(window_get_root_layer(window),bitmap_layer_get_layer(s_flower_anim_layer_s10));
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_flower_anim_layer_m01));
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_flower_anim_layer_m10));
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_flower_anim_layer_m01));
+  layer_add_child(window_get_root_layer(window),bitmap_layer_get_layer(s_flower_anim_layer_s10));
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_flower_anim_layer_s01));
   //*/
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer)); 
+  //*/
   
   // Make sure the time is displayed from the start
+  lastSec=61;
+  lastMin=61;
   update_time();
 }
 
@@ -155,22 +214,25 @@ static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
+  
+  bitmap_layer_destroy(s_background_layer);
+  gbitmap_destroy(s_background_bitmap);
+  
   bitmap_layer_destroy(s_character_layer);
   gbitmap_destroy(s_character_bitmap);
-  //*
+  
   bitmap_layer_destroy(s_flower_base_layer);
   gbitmap_destroy(s_flower_base_bitmap);
-  //*/
-  /*/
+  
   bitmap_layer_destroy(s_flower_anim_layer_s01);
   bitmap_layer_destroy(s_flower_anim_layer_s10);
   bitmap_layer_destroy(s_flower_anim_layer_m01);
   bitmap_layer_destroy(s_flower_anim_layer_m10);
+  
   gbitmap_destroy(s_flower_anim_bitmap_s01);
   gbitmap_destroy(s_flower_anim_bitmap_s10);
   gbitmap_destroy(s_flower_anim_bitmap_m01);
   gbitmap_destroy(s_flower_anim_bitmap_m10);
-  //*/
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
